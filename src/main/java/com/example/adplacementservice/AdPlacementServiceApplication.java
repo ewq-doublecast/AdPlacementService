14d1a661b4aabb7e5377946a97264af9e83ff8a3
Nikolai Evtushenko <109798979+ewq-doublecast@users.noreply.github.com>
2024-04-30 14:58:44 +0300

    Initial commit

---
+++ src/main/java/com/example/adplacementservice/AdPlacementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.adplacementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdPlacementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdPlacementServiceApplication.class, args);
+    }
+
+}
